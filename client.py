2c4a0624654ccd14ab1f1be02001fe1188e25565
Anton <antongorynya1@gmail.com>
2024-03-23 13:26:26 +0300

    Save messages history to file.

--- client.py
+++ client.py
@@ -1,17 +1,39 @@
 import asyncio
+from datetime import datetime
+import aiofiles
+import logging
 
 
-async def tcp_echo_client(message):
-    reader, writer = await asyncio.open_connection(
-        'minechat.dvmn.org', 5000)
-    data = b''
-    while True:
-        data += await reader.read(100)
-        if b'\n' in data:
-            print(f'decode: {data.decode()}')
-            data = b''
+async def read_chat(adress, port):
+    try:
+        reader, writer = await asyncio.open_connection(adress, port)
+        data = b''
+        while True:
+            data += await reader.read(100)
+            if b'\n' in data:
+                now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                message = f'[{now}] {data.decode()}'
+                logging.debug(message)
+                yield message
+                data = b''
+    except (ConnectionResetError, asyncio.exceptions.IncompleteReadError):
+        logging.error("Соединение разорвано.")
+    finally:
+        writer.close()
+        await writer.wait_closed()
+        yield None
+
+async def write_chat_log(adress, port, file_name='log_file.txt'):
+    async with aiofiles.open(file_name, 'a') as file:
+        async for message in read_chat(adress, port):
+            await file.write(message)
 
 
 
 if __name__ == '__main__':
-    asyncio.run(tcp_echo_client('Hello World!'))
+    logging.basicConfig(level=logging.DEBUG)
+    #asyncio.run(read_chat('minechat.dvmn.org', 5000, log=True))
+    try:
+        asyncio.run(write_chat_log('minechat.dvmn.org', 5000))
+    except Exception as e:
+        print(e)
